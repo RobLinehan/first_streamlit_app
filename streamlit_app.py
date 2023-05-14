cdcd1c7c6768cf453874e8d54a23f3da8c10c2e6
RobLinehan <133597456+RobLinehan@users.noreply.github.com>
2023-05-14 15:15:03 -0400

    Update streamlit_app.py
    updating the table, to only show the selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # put a pick list, allowing user to select which fruit they want
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
