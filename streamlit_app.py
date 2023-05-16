5aacce2b27e3fc9ce89bed033d381f561cef190c
RobLinehan <133597456+RobLinehan@users.noreply.github.com>
2023-05-15 22:57:04 -0400

    Update streamlit_app.py
    adding output of input box

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,4 @@ streamlit.dataframe(my_data_rows)
 
 #Input new fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
