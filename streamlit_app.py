9ff0d8ba543ac4fe57ce31c4de7c103534efa004
RobLinehan <133597456+RobLinehan@users.noreply.github.com>
2023-05-15 23:41:12 -0400

    Update streamlit_app.py
    removing additional insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,5 @@ if streamlit.button('Add a Fruit to the List'):
 
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 
 
